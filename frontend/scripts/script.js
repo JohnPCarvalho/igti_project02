08fdf6c215a65a10a3b27591c6ec49a3215cecf7
JohnPCarvalho <johnnypcarvalho@gmail.com>
2021-03-17 09:14:40 -0300

    refactoring fetchUsers function

--- frontend/scripts/script.js
+++ frontend/scripts/script.js
@@ -1,50 +1,34 @@
-const fetchUsers = async () => {
+let arrayOfUsers = [];
+
+
+async function fetchUsers () {
   let resource = await fetch("http://localhost:3001/users");
   let users = await resource.json();
-  return users;
+  arrayOfUsers = users.map(user => 
+      ({
+        name: user.name.first,
+        picture: user.picture.thumbnail,
+        dob: user.dob.date,
+        email: user.email
+      })
+    );
+    return arrayOfUsers;
 }
 
-window.addEventListener('load', async () => {
-  activateInput();
-  let usersList = await fetchUsers();
-  console.log(usersList);
-  let mappedUsers = mapUsers(usersList);
-  console.log(mappedUsers);
-  activateInput();
+function search () {
+  //captures user input
+  userInput.addEventListener('keyup', (event) => {
+    if (event.key === 'Enter') {
+      console.log(arrayOfUsers);
+      arrayOfUsers.filter((user) => {
+        return user.name.toLowerCase().includes(event.target.value).toLowerCase();
+      })
+    }
+  })
+}
+
+window.addEventListener('load', () => {
   
-  //filterUsers(mappedUsers);
+  fetchUsers();
+  search();
 });
-
-let userInput = document.querySelector("#userInput");
-let form = document.querySelector("form");
-
-
-const mapUsers = (usersList) => {
-  return usersList.map(user => 
-    ({
-      name: user.name.first,
-      picture: user.picture.thumbnail,
-      dob: user.dob.date,
-      email: user.email
-    })
-  )
-}
-
-const filterUsers = (usersList) => {
-  console.log(usersList);
-
-  //function filterName(arrayList) {
- //   return arrayList.name.first.charAt(0) == 'B';
- // }
-
-  let filteredList = usersList.filter((names) => {
-    return names.name.first.charAt(0) == 'A';
-  });
-  console.log(filteredList);
-}
-
-const activateInput = () => {
-  userInput.addEventListener('input',  () => {
-    console.log('logou');
-  });
-}
